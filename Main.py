820d7cb77555abb3cd0af7a4eb81fe78512d6acb
hugolucas <hugojlucas@gmail.com>
2017-11-05 18:27:08 -0800

    closes #1

--- Main.py
+++ Main.py
@@ -7,5 +7,4 @@ topo = GraphTopology.GraphTopology(k_val=8, n_p=NUM_VM_PAIRS, n_mb=0, max_vm_siz
 topo.create_topology()
 
 sim = GraphSimulator.GraphSimulator(topo, NUM_VM_PAIRS)
-sim.set_up_data_center()
-sim.revert_state()
+

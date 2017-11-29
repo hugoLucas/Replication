bf6a694cec1c1c4691f784efc3bf815de3773cf4
hugoLucas <hugojlucas@gmail.com>
2017-11-28 17:05:48 -0800

    Graph code added, has not been tested

--- Main.py
+++ Main.py
@@ -7,16 +7,13 @@ import GraphTopology
 
 import matplotlib.pyplot as plt
 
-import time
 import numpy as np
 import scipy as sp
 import scipy.stats
 
-NUM_VM_PAIRS = 50
 MAX_VM_SIZE = 1
-NUM_SIMS = 50
-HOST_CAP = 9
-K_VAL = 4
+NUM_SIMS = 5
+K_VAL = 8
 
 
 def mean_confidence_interval(data, confidence=0.95):
@@ -32,79 +29,86 @@ def calc_mean_and_width(data):
 
     return data_m, np.abs(data_m - data_t)
 
-base_data, greedy_data, cluster_data, mem_reduc_data, exhas_data = [], [], [], [], []
-greedy_time, cluster_time, mem_reduc_time, exhas_time = 0, 0, 0, 0
-for i in range(0, NUM_SIMS):
-    topo = GraphTopology.GraphTopology(k_val=K_VAL, n_p=NUM_VM_PAIRS, n_mb=0, max_vm_size=MAX_VM_SIZE,
-                                       phys_host_cap=HOST_CAP)
-    topo.create_topology()
 
-    sim = GraphSimulator.GraphSimulator(topo, NUM_VM_PAIRS)
-    sim.set_up_data_center()
+def run_simulation(num_vm_pairs, host_capacity):
+    bd, gd, cd, mrd, ehd = [], [], [], [], []
+    for i in range(0, NUM_SIMS):
+        topo = GraphTopology.GraphTopology(k_val=K_VAL, n_p=num_vm_pairs, n_mb=0, max_vm_size=MAX_VM_SIZE,
+                                           phys_host_cap=host_capacity)
+        topo.create_topology()
 
-    base_data.append(sim.get_base_cost())
+        sim = GraphSimulator.GraphSimulator(topo, num_vm_pairs)
+        sim.set_up_data_center()
+        bd.append(sim.get_base_cost())
 
-    greedy_start = time.time()
-    greedy_data.append(sim.pass_algorithm(GreedyAlgorithm.GreedyAlgorithm))
-    greedy_end = time.time()
-    greedy_time += greedy_end - greedy_start
-    sim.revert_state()
+        gd.append(sim.pass_algorithm(GreedyAlgorithm.GreedyAlgorithm))
+        sim.revert_state()
 
-    cluster_start = time.time()
-    cluster_data.append(sim.pass_algorithm(ClusterAlgorithm.ClusterAlgorithm))
-    cluster_end = time.time()
-    cluster_time += cluster_end - cluster_start
-    sim.revert_state()
+        cd.append(sim.pass_algorithm(ClusterAlgorithm.ClusterAlgorithm))
+        sim.revert_state()
 
-    mem_reduc_start = time.time()
-    mem_reduc_data.append(sim.pass_algorithm(MemoryReduction.MemoryReduction))
-    mem_reduc_end = time.time()
-    mem_reduc_time += mem_reduc_end - mem_reduc_start
-    sim.revert_state()
+        mrd.append(sim.pass_algorithm(MemoryReduction.MemoryReduction))
+        sim.revert_state()
 
-    exhas_start = time.time()
-    exhas_data.append(sim.pass_algorithm(ExhastiveSearch.ExhastiveSearch))
-    exhas_end = time.time()
-    exhas_time += exhas_end - exhas_start
+        ehd.append(sim.pass_algorithm(ExhastiveSearch.ExhastiveSearch))
 
-    print "SIM {} done...".format(str(i + 1))
-
-print "GREEDY: {}s".format(greedy_time)
-print "CLUSTER: {}s".format(cluster_time)
-print "MEM REDUC: {}s".format(mem_reduc_time)
-print "EXHAS: {}s".format(exhas_time)
-
-base_mean, base_width = calc_mean_and_width(base_data)
-greedy_mean, greedy_width = calc_mean_and_width(greedy_data)
-cluster_mean, cluster_width = calc_mean_and_width(cluster_data)
-mem_reduc_mean, mem_reduc_width = calc_mean_and_width(mem_reduc_data)
-exhas_mean, exhas_width = calc_mean_and_width(exhas_data)
-
-print "GREEDY: {} cost".format(greedy_mean)
-print "CLUSTER: {} cost".format(cluster_mean)
-print "MEM REDUC: {} cost".format(mem_reduc_mean)
-print "EXHAS: {} cost".format(exhas_mean)
-
-plt.bar(1, base_mean, yerr=base_width, capsize=5, edgecolor="red", hatch="+", fill=False)
-plt.bar(2, greedy_mean, yerr=greedy_width, capsize=5, edgecolor="navy", hatch="/", fill=False)
-plt.bar(3, cluster_mean, yerr=cluster_width, capsize=5, edgecolor="green", hatch="x", fill=False)
-plt.bar(4, mem_reduc_mean, yerr=mem_reduc_width, capsize=5, edgecolor="magenta", hatch=".", fill=False)
-plt.bar(5, exhas_mean, yerr=exhas_width, capsize=5, edgecolor="black", hatch="-", fill=False)
+        print "SIM {}.{} done...".format(str(num_vm_pairs), str(i + 1))
+    return bd, gd, cd, mrd, ehd
 
 
-plt.xticks([1, 2, 3, 4, 5], ['Base', 'Greedy', 'Cluster', 'MemReduce', "Exhas"], fontsize=20)
-font = {'family': 'normal', 'weight': 'bold', 'size': 15}
-plt.rc('font', **font)
+def data_set_mean(b, g, c, m, e):
+    b_m, b_w = calc_mean_and_width(b)
+    g_m, g_w = calc_mean_and_width(g)
+    c_m, c_w = calc_mean_and_width(c)
+    m_m, m_w = calc_mean_and_width(m)
+    e_m, e_w = calc_mean_and_width(e)
 
-plt.gca().minorticks_on()
-plt.gca().grid(b=True, which='major', color='black', linestyle=':')
-plt.gca().grid(b=True, which='minor', color='black', linestyle=':', linewidth=0.5)
-plt.gca().set_axisbelow(True)
-plt.yticks(fontsize=20)
-plt.gca().set_yticks(plt.gca().get_yticks()[::2])
-# plt.ylabel('Total Energy Cost of VM Pairs', fontsize=30)
-# plt.xlabel('Resource Capacity of PMs', fontsize=35)
-plt.show()
+    return b_m, b_w, g_m, g_w, c_m, c_w, m_m, m_w, e_m, e_w
+
+
+def plot_subset(mean_vector, width_vector, offset, bar_color, bar_hatch):
+    current_offset = offset
+    for mean_val, width_val in zip(mean_vector, width_vector):
+        plt.bar(current_offset, mean_val, yern=width_val, capsize=5, edgecolor=bar_color, hatch=bar_hatch, fill=False)
+        offset += 6
+
+
+def graph_simulation(capacity, title_x, title_y):
+    b1, g1, c1, m1, e1 = run_simulation(500, capacity)
+    b2, g2, c2, m2, e2 = run_simulation(1000, capacity)
+    b3, g3, c3, m3, e3 = run_simulation(1500, capacity)
+    b4, g4, c4, m4, e4 = run_simulation(2000, capacity)
+
+    b1m, b1w, g1m, g1w, c1m, c1w, m1m, m1w, e1m, e1w = data_set_mean(b1, g1, c1, m1, e1)
+    b2m, b2w, g2m, g2w, c2m, c2w, m2m, m2w, e2m, e2w = data_set_mean(b2, g2, c2, m2, e2)
+    b3m, b3w, g3m, g3w, c3m, c3w, m3m, m3w, e3m, e3w = data_set_mean(b3, g3, c3, m3, e3)
+    b4m, b4w, g4m, g4w, c4m, c4w, m4m, m4w, e4m, e4w = data_set_mean(b4, g4, c4, m4, e4)
+
+    bm_data, bw_data = [b1m, b2m, b3m, b4m], [b1w, b2w, b3w, b4w]
+    gm_data, gw_data = [g1m, g2m, g3m, g4m], [g1w, g2w, g3w, g4w]
+    cm_data, cw_data = [c1m, c2m, c3m, c4m], [c1w, c2w, c3w, c4w]
+    mm_data, mw_data = [m1m, m2m, m3m, m4m], [m1w, m2w, m3w, m4w]
+    em_data, ew_data = [e1m, e2m, e3m, e4m], [e1w, e2w, e3w, e4w]
+
+    plot_subset(bm_data, bw_data, 1, "red", "+")
+    plot_subset(gm_data, gw_data, 2, "navy", "/")
+    plot_subset(cm_data, cw_data, 3, "green", "x")
+    plot_subset(mm_data, mw_data, 4, "magenta", ".")
+    plot_subset(em_data, ew_data, 5, "black", "-")
+
+    plt.xticks([3, 9, 15, 21, 27], ['500', '1000', '1500', '2000'], fontsize=20)
+    font = {'family': 'normal', 'weight': 'bold', 'size': 15}
+    plt.rc('font', **font)
+
+    plt.gca().minorticks_on()
+    plt.gca().grid(b=True, which='major', color='black', linestyle=':')
+    plt.gca().grid(b=True, which='minor', color='black', linestyle=':', linewidth=0.5)
+    plt.gca().set_axisbelow(True)
+    plt.yticks(fontsize=20)
+    plt.gca().set_yticks(plt.gca().get_yticks()[::2])
+    plt.ylabel(title_x, fontsize=30)
+    plt.xlabel(title_y, fontsize=35)
+    plt.show()
 
 
 
